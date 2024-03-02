5c2f9508d9f0118572ac9783fd297c687c8f06e7
BortoAle <busta_08_strillone@icloud.com>
2024-03-02 16:12:15 +0100

    Rename to OnboardingKit

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Onboarding",
+    name: "OnboardingKit",
     platforms: [.iOS(.v17), .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Onboarding",
-            targets: ["Onboarding"]),
+            name: "OnboardingKit",
+            targets: ["OnboardingKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Onboarding"),
+            name: "OnboardingKit"),
     ]
 )
